ec894e6d9c97782b275ca28b543545ef3f08da79
ZhegrovaShkurte <147830779+ZhegrovaShkurte@users.noreply.github.com>
2023-10-25 15:38:31 +0200

    Update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
                 <div class="col-md-3">
-                <b>Home Page.</b>
+                <b>Home Page..</b>
                 <div class="row mb-0">
                     <div class="col-md-6">
                 <a href="/edit" class="btn btn-success">Edit</a>
